02abcffb60aace70631691b207ae60273f9a755b
Tuan Tran <mptnt1988@gmail.com>
2018-02-06 09:55:12 +0700

    Init mix project

---
+++ lib/ereceipt.ex
@@ -0,0 +1,18 @@
+defmodule Ereceipt do
+  @moduledoc """
+  Documentation for Ereceipt.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ereceipt.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ereceipt_test.exs
@@ -0,0 +1,8 @@
+defmodule EreceiptTest do
+  use ExUnit.Case
+  doctest Ereceipt
+
+  test "greets the world" do
+    assert Ereceipt.hello() == :world
+  end
+end
