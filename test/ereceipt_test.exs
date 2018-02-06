b2303d00544f76a2a31e113513d9bdfe93c8cbe9
Tuan Tran <mptnt1988@gmail.com>
2018-02-06 10:25:19 +0700

    Add failed test cases

--- test/ereceipt_test.exs
+++ test/ereceipt_test.exs
@@ -12,8 +12,112 @@ defmodule EreceiptTest do
   use ExUnit.Case
   doctest Ereceipt
 
-  test "dummy test case" do
-    assert Ereceipt.main([]) == nil
+  test "exempt, not imported" do
+    input =
+      [["Quantity", "Product", "Price"],
+       [1, "book", 12.49]]
+    categories =
+      [["Product", "Category", "Imported"],
+       ["book", "book", ""]]
+    tax =
+      "[tax]\n" <>
+      "exempt = book, food, medical\n" <>
+      "basic_rate = 10%\n" <>
+      "import_duty = 5%\n"
+    expected_output =
+      %{"Amount" => "12.49",
+        "Category" => "book",
+        "Price" => "12.49",
+        "Product" => "book",
+        "Quantity" => "1",
+        "Sales Tax" => "0.00",
+        "Total Amount" => "12.49"}
+    case_config = %{input: input,
+                    categories: categories,
+                    tax: tax,
+                    expected: expected_output}
+    run_case(case_config)
+  end
+
+  test "not exempt, not imported" do
+    input =
+      [["Quantity", "Product", "Price"],
+       [2, "music cd", 14.99]]
+    categories =
+      [["Product", "Category", "Imported"],
+       ["music cd", "entertainment", ""]]
+    tax =
+      "[tax]\n" <>
+      "exempt = book, food, medical\n" <>
+      "basic_rate = 10%\n" <>
+      "import_duty = 5%\n"
+    expected_output =
+      %{"Amount" => "29.98",
+        "Category" => "entertainment",
+        "Price" => "14.99",
+        "Product" => "music cd",
+        "Quantity" => "2",
+        "Sales Tax" => "3.00",
+        "Total Amount" => "32.98"}
+    case_config = %{input: input,
+                    categories: categories,
+                    tax: tax,
+                    expected: expected_output}
+    run_case(case_config)
+  end
+
+  test "not exempt, imported" do
+    input =
+      [["Quantity", "Product", "Price"],
+       [3, "imported perfume", 27.99]]
+    categories =
+      [["Product", "Category", "Imported"],
+       ["imported perfume", "fragrance", "TRUE"]]
+    tax =
+      "[tax]\n" <>
+      "exempt = book, food, medical\n" <>
+      "basic_rate = 10%\n" <>
+      "import_duty = 5%\n"
+    expected_output =
+      %{"Amount" => "83.97",
+        "Category" => "fragrance",
+        "Price" => "27.99",
+        "Product" => "imported perfume",
+        "Quantity" => "3",
+        "Sales Tax" => "12.60",
+        "Total Amount" => "96.57"}
+    case_config = %{input: input,
+                    categories: categories,
+                    tax: tax,
+                    expected: expected_output}
+    run_case(case_config)
+  end
+
+  test "exempt, imported" do
+    input =
+      [["Quantity", "Product", "Price"],
+       [4, "imported chocolate", 10]]
+    categories =
+      [["Product", "Category", "Imported"],
+       ["imported chocolate", "food", "TRUE"]]
+    tax =
+      "[tax]\n" <>
+      "exempt = book, food, medical\n" <>
+      "basic_rate = 10%\n" <>
+      "import_duty = 5%\n"
+    expected_output =
+      %{"Amount" => "40.00",
+        "Category" => "food",
+        "Price" => "10.00",
+        "Product" => "imported chocolate",
+        "Quantity" => "4",
+        "Sales Tax" => "2.00",
+        "Total Amount" => "42.00"}
+    case_config = %{input: input,
+                    categories: categories,
+                    tax: tax,
+                    expected: expected_output}
+    run_case(case_config)
   end
 
   defp run_case(config) do
